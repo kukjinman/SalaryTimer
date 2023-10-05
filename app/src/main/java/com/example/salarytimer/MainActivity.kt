8f6967b21364f1eaa9b916799f7a72e0e3d42c66
kukjin <thater114@gmail.com>
2023-10-05 23:18:03 +0900

    fragment switching applied

--- app/src/main/java/com/example/salarytimer/MainActivity.kt
+++ app/src/main/java/com/example/salarytimer/MainActivity.kt
@@ -2,10 +2,35 @@ package com.example.salarytimer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.salarytimer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binding.bottomNav.setOnItemSelectedListener {item ->
+            when(item.itemId){
+                R.id.home -> {
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.fragmentFrame, mainFragment())
+                        .commit()
+                }
+                R.id.setting -> {
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.fragmentFrame, settingFragment())
+                        .commit()
+
+                }
+            }
+            true
+        }
+
     }
 }
